7feba8472e7e8ede68a9b47017a10c7b2cf1b919
terurou <terurou@denkiyagi.jp>
2018-03-29 20:12:37 +0900

    Add ReadOnlyDynamic and ReadOnlyArray

---
+++ src/externtype/ReadOnlyArray.hx
@@ -0,0 +1,8 @@
+package externtype;
+
+@:forward(length, concat, join, toString, indexOf, lastIndexOf, copy, map, filter, iterator)
+abstract ReadOnlyArray<T>(Array<T>) from Array<T> {
+    @:arrayAccess inline function get(i: Int) {
+        return this[i];
+    }
+}
